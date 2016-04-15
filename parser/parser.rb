86f4a67871f4099feefae4a4b4ea054e7c306229
Mark Keele <m.keele88@gmail.com>
2016-04-14 23:08:57 -0400

    uniform in set, clean up

--- parser/parser.rb
+++ parser/parser.rb
@@ -1,13 +1,17 @@
 require 'rest-client'
 require 'json'
 
+# get distinct fingerprints from db
 url = 'http://dev.keele.me/fingers/api/distinct'
+# get user fingerprints from db
+# url = 'http://dev.keele.me/fingers/api/fingers/mkeele'
+
 response = RestClient.get(url)
 res = JSON.parse(response)
 
 # all data
-# train = [[]]
-train = Array.new(48) { Array.new(8) }
+train = Array.new(res.count) { Array.new(8) }
+
 # data by feature
 user_agent = []
 language = []
@@ -20,97 +24,85 @@ js_fonts = []
 
 i = 0
 j = 0
+# each response
 res.each do |fp|
+  # each fingerprint
   fp.each do |key, value|
-    if key == 'username'
-      # puts value
-    end
-
     if key == 'components'
       value.each do |comp|
         if comp["key"] == 'user_agent'
           # string
-          # puts comp["value"].gsub(/\s+/, "").strip
           train[i][j] = comp["value"].gsub(/\s+/, "").strip
           j += 1
           user_agent.push(comp["value"].gsub(/\s+/, "").strip)
         end
         if comp["key"] == 'language'
           # string
-          # puts comp["value"].gsub(/\s+/, "").strip
           train[i][j] = comp["value"].gsub(/\s+/, "").strip
           j += 1
           language.push(comp["value"].gsub(/\s+/, "").strip)
         end
         if comp["key"] == 'color_depth'
           # integer
-          # puts comp["value"].to_s.strip
           train[i][j] = comp["value"].to_s.strip
           j += 1
           color_depth.push(comp["value"].to_s.strip)
         end
         if comp["key"] == 'resolution'
           # array
-          # puts comp["value"].join('').strip
           train[i][j] = comp["value"].join('').strip
           j += 1
           resolution.push(comp["value"].join('').strip)
         end
         if comp["key"] == 'timezone_offset'
           # integer
-          # puts comp["value"].to_s.strip
           train[i][j] = comp["value"].to_s.strip
           j += 1
           timezone_offset.push(comp["value"].to_s.strip)
         end
         if comp["key"] == 'navigator_platform'
           # string
-          # puts comp["value"].gsub(/\s+/, "").strip
           train[i][j] = comp["value"].gsub(/\s+/, "").strip
           j += 1
           navigator_platform.push(comp["value"].gsub(/\s+/, "").strip)
         end
         if comp["key"] == 'regular_plugins'
           # array
-          # puts comp["value"].sort.join('').gsub(/\s+/, "").strip
           train[i][j] = comp["value"].sort.join('').gsub(/\s+/, "").strip
           j += 1
           regular_plugins.push(comp["value"].sort.join('').gsub(/\s+/, "").strip)
         end
         if comp["key"] == 'js_fonts'
           # array
-          # puts comp["value"].sort.join('').gsub(/\s+/, "").strip
           train[i][j] = comp["value"].sort.join('').gsub(/\s+/, "").strip
           j += 1
           js_fonts.push(comp["value"].sort.join('').gsub(/\s+/, "").strip)
         end
       end
     end
+  end # end fingerprint
+  i += 1
+  j = 0
+end # end response
+
+i = 0
+j = 0
+uniform = []
+while i < train.length
+  while j < train.length
+    if i != j and train[i] == train[j]
+      uniform.push(i)
+    end
+    j += 1
   end
   i += 1
   j = 0
 end
 
-# puts train.length
-# i = 0
-# j = 0
-# same = 0
-# while i < train.length
-#   while j < train.length
-#     if i != j and train[i] == train[j]
-#       puts "HERE"
-#       puts i
-#       puts j
-#       puts train[i]
-#       puts train[j]
-#       same += 1
-#     end
-#     j += 1
-#   end
-#   i += 1
-#   j = 0
-# end
-# puts same
+puts 'fingerprint: uniform in set / total in set'
+puts 'uniform fp: ' + uniform.count.to_s + ' / ' + train.count.to_s
+puts 'uniform fp number: ' + uniform.join(' ')
+puts "\n"
 
 puts 'feature: unique in set / total in set'
 puts 'user_agent: ' + user_agent.uniq.count.to_s + ' / ' + user_agent.count.to_s
